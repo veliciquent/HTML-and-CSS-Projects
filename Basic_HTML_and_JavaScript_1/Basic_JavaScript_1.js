85059c1d430650d0a9389f08449e8cc5f1df439d
Joshua Ramos <veliciquent@gmail.com>
2021-03-02 02:04:24 -0700

    Adding basic HTML and JavaScript to repository

---
+++ Basic_HTML_and_JavaScript_1/Basic_JavaScript_1.js
@@ -0,0 +1,6 @@
+alert("Hello from the Other Side");
+function My_First_Function() {
+	var String = "Kiss me, I'm Irish!";
+	var result = String.fontcolor("green");
+	document.getElementById("Irish").innerHTML = result;
+}
